32f57eb880d0253b62c42bfbe7a639c45c83442d
오소진 <jaisonoh@naver.com>
2017-03-06 23:39:33 +0900

    Complete overall page

--- netcs/urls.py
+++ netcs/urls.py
@@ -15,11 +15,20 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
+    url(r'^events$', TemplateView.as_view(template_name="events.html"), name='events'),
+    url(r'^members$', TemplateView.as_view(template_name="members.html"), name='members'),
+    url(r'^research$', TemplateView.as_view(template_name="research.html"), name='research'),
+    url(r'^projects$', TemplateView.as_view(template_name="projects.html"), name='projects'),
+    url(r'^publications$', TemplateView.as_view(template_name="publications.html"), name='publications'),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
